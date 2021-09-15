47f71b02e5daf455b4aef8ccc8286283c7c6c792
Junel <sacrojunel@gmail.com>
2021-09-15 18:34:01 +0800

    rightbar profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -27,8 +27,7 @@ export default function Profile() {
                     </div>
                     <div className="profileRightBottom">
                         <Feed/>
-                        <Rightbar/>
-                       
+                        <Rightbar profile/>                       
                     </div>
                 </div>              
             </div>
