a8c7905510e4cd28e4d9b51cbb86de5ecde07112
Junel <sacrojunel@gmail.com>
2021-09-15 18:32:18 +0800

    ok right bar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -100,7 +100,6 @@ export default function Rightbar({ profile }) {
             <div className="rightBarWrapper"> 
                         { profile ? <ProfileRightBar/>:<HomeRightBar/> }                              
             </div>
-        </div> 
-       
+        </div>        
     )
 }
