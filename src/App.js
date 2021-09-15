97d3fa72782469072b40363fe20739bbe608e61f
Junel <sacrojunel@gmail.com>
2021-09-15 18:35:56 +0800

    <home page> /app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-// import Home from "./pages/home/Home";
-
-import Login from "./pages/login/Login";
+ import Home from "./pages/home/Home";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
 // import Profile from "./pages/profile/Profile";
-
-
-
 
 
 function App() {
   return (
     <div>
-        <Login/>
+        <Home/>
     </div>  
   );
 }
